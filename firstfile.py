635f02fefc7a89e80e42300312240410a2977224
matthieumoreau0 <matthieumoro92@gmail.com>
2019-12-10 13:12:21 +0100

    Documentation of the functions

--- firstfile.py
+++ firstfile.py
@@ -8,6 +8,15 @@ import matplotlib.pyplot as plt
 
 
 def dct2(block,cos_t):
+  '''
+  Computes the dct2 of a 2D array
+
+  Parameters:
+  block (np.array of floats of shape (w,h)):
+
+  Returns:
+  np.array of float with shape (w,h): 2D dct2 of the block parameter
+  '''
   dct_num=np.zeros(block.shape)
   dct_test = dct(dct(block.T, norm='ortho').T, norm='ortho')
   # for i in range(8): #### Sanity check
@@ -26,6 +35,15 @@ def dct2(block,cos_t):
   return dct_test
 
 def computeVotes(L):
+  '''
+  Computes for each pixel the best 8x8 jpeg grid
+
+  Parameters:
+  L (np.array of floats with shape(w,h): luminance of an image)
+
+  Returns:
+  np.array of int with shape(w,h): Best 8x8 grid for each pixel, if best grid is (i,j) the value in the array is i%8 * 8 + j%8
+  '''
   votes=np.full(L.shape,-1)
   zeros=np.zeros(L.shape)
   cos_t=np.zeros((8,8))
@@ -47,96 +65,156 @@ def computeVotes(L):
   return votes
 
 def getLuminance(I):
-    return np.average(image,2,weights=[0.2126,0.7152,0.0722]).astype("float32")
+  '''
+  Returns the luminance of image I with weights [0.2126,0.7152,0.0722]
+
+  Parameters:
+  I (array of shape(w,h,3)): Image with RGB channels
+
+  Returns:
+  np.array of shape (w,h): Luminance of I 
+  '''
+  return np.average(image,2,weights=[0.2126,0.7152,0.0722]).astype("float32")
 
 def binomTail(n,k,p):
-    sum = 0
-    test = 1-scipy.stats.binom.cdf(k-1,n,p)
-    return test
+  '''
+  Returns the tail of order k of a binomial distribution B(n,p)
+
+  Parameters: 
+  n (int) 
+  k (int) 
+  p (float) : probability 0<=p<=1
+  
+  Returns:
+  float: tail of order k of a binomial distribution B(n,p)
+  '''
+  return 1-scipy.stats.binom.cdf(k-1,n,p)
 
 def gridDetection(I):
-    X,Y,C = np.shape(I)
-    L = getLuminance(I)
-    votes = computeVotes(L)
-    histo = np.histogram(votes,range(-1,65))
-    bestGrid = np.argmax(histo[0][1:])
-    bestValue = np.max(histo[0])
+  '''
+  Returns the best 8x8 jpeg grid for image I
 
-    NFA = 64*X*Y*np.sqrt(X*Y)*binomTail(int(X*Y/64),int(bestValue/64),1/64.)
-    if NFA<1 :
-        return bestGrid,NFA,votes
-    else :
-        return -1,NFA,votes
+  Parameters:
+  I (image of shape(w,h,3)): Image to study
+
+  Returns:
+  bestGrid (int): index of the best grid for that image -1 if no significant grid is detected
+  NFA (float): value of the NFA for that grid
+  votes (array of int with shape (w,h)): best grid for each pixel
+  '''
+  X,Y,C = np.shape(I)
+  L = getLuminance(I)
+  votes = computeVotes(L)
+  histo = np.histogram(votes,range(-1,65))
+  bestGrid = np.argmax(histo[0][1:])
+  bestValue = np.max(histo[0])
+
+  NFA = 64*X*Y*np.sqrt(X*Y)*binomTail(int(X*Y/64),int(bestValue/64),1/64.)
+  if NFA<1 :
+      return bestGrid,NFA,votes
+  else :
+      return -1,NFA,votes
 
 def regionGrowing(votes,seed,W):
-    queue = [seed]
-    visited = np.zeros(votes.shape)
-    region= [seed]
-    while len(queue)>0 :
-        x,y=queue.pop(0)
-        visited[x,y]=True
-        for i in range(-W,W+1):
-            for j in range(-W,W+1):
-                if (i==0 and j==0) or x+i<0 or x+i>=np.shape(votes)[0] \
-                or y+j<0 or y+j>= np.shape(votes)[1]:
-                    continue
-                elif not visited[x+i,y+j] and votes[x+i,y+j]==votes[x,y] :
-                    visited[x+i,y+j]=True
-                    region.append([x+i,y+j])
-                    queue.append([x+i,y+j])
-    return region
+  """
+  Computes the region of pixels with same vote as the seed (we look for pixel in a window (W,W) to grow the region
+
+  Parameters:
+  votes (2D-array of int with shape (w,h)): best grid for each pixel
+  seed (tuple of int): pixel to grow the region around
+  W (int): size of the window to grow the region
+
+  Returns:
+  array of int: list of pixels belonging to the region 
+  """
+  queue = [seed]
+  visited = np.zeros(votes.shape)
+  region= [seed]
+  while len(queue)>0 :
+      x,y=queue.pop(0)
+      visited[x,y]=True
+      for i in range(-W,W+1):
+          for j in range(-W,W+1):
+              if (i==0 and j==0) or x+i<0 or x+i>=np.shape(votes)[0] \
+              or y+j<0 or y+j>= np.shape(votes)[1]:
+                  continue
+              elif not visited[x+i,y+j] and votes[x+i,y+j]==votes[x,y] :
+                  visited[x+i,y+j]=True
+                  region.append([x+i,y+j])
+                  queue.append([x+i,y+j])
+  return region
 
 
 
 
-def BoundingBox(R):
-    R = np.transpose(R)
-    # print("R",R)
-    xmin = np.min(R[0,:])
-    xmax = np.max(R[0,:])
-    ymin = np.min(R[1,:])
-    ymax = np.max(R[1,:])
+def boundingBox(R):
+  '''
+  Computes the bounding box of a list of pixel indexes
 
-    # print("MAX",xmin,xmax,ymin,ymax)
+  Parameters:
+  R (list of tuple of int)
 
-    return xmin,xmax,ymin,ymax
+  Returns:
+  (int,int,int,int): bounding box of the region
+  '''
+  R = np.transpose(R)
+  # print("R",R)
+  xmin = np.min(R[0,:])
+  xmax = np.max(R[0,:])
+  ymin = np.min(R[1,:])
+  ymax = np.max(R[1,:])
+
+  # print("MAX",xmin,xmax,ymin,ymax)
+
+  return xmin,xmax,ymin,ymax
 
 
 def forgeryDetection(votes,G,W):
-    forgerMask=np.zeros(np.shape(votes))
-    X,Y = np.shape(votes)
+  '''
+  Computes the forgery zones
 
-    for x in tqdm.tqdm(range(len(forgerMask))):
-        for y in range(len(forgerMask[0])):
-            if votes[x,y]>-1 and votes[x,y]!=G:
-                R = regionGrowing(votes,[x,y],W)
-                if len(R)<4:
-                    continue
-                xmin,xmax,ymin,ymax = BoundingBox(R)
-                Bx =  (xmax-xmin)+1
-                By = ymax-ymin+1
-                N = max(xmax-xmin+1,ymax-ymin+1)
-                card = len(R)
-                #NFA = 64 *X*Y*np.sqrt(X*Y)*binomTail(int(N*N/64),int(card/64),1/64) formule fausse du papier
-                NFA = 64 *Bx*By*np.sqrt(Bx*By)*binomTail(int(N*N/64),int(card/64),1/64.0)
-                if NFA < 1 :
-                    # forgerMask[np.transpose(R)]=True
-                    for i,j in R:
-                        forgerMask[i,j]=True
-                for i,j in R :
-                    votes[i,j] = -1
+  Parameters:
+  votes (2D array of int): best grid for each pixel
+  G (int): main grid of the image 
+  W (int): size of the window for the region growing
 
-                    # forgerMask[np.transpose(R)]=True
-    return forgerMask
+  Returns:
+  2D array of bool: Binary image expressing whether each pixel has been detected as forged
+  '''
+  forgerMask=np.zeros(np.shape(votes))
+  X,Y = np.shape(votes)
+
+  for x in tqdm.tqdm(range(len(forgerMask))):
+      for y in range(len(forgerMask[0])):
+          if votes[x,y]>-1 and votes[x,y]!=G:
+              R = regionGrowing(votes,[x,y],W)
+              if len(R)<4:
+                  continue
+              xmin,xmax,ymin,ymax = boundingBox(R)
+              Bx =  (xmax-xmin)+1
+              By = ymax-ymin+1
+              N = max(xmax-xmin+1,ymax-ymin+1)
+              card = len(R)
+              #NFA = 64 *X*Y*np.sqrt(X*Y)*binomTail(int(N*N/64),int(card/64),1/64) formule fausse du papier
+              NFA = 64 *Bx*By*np.sqrt(Bx*By)*binomTail(int(N*N/64),int(card/64),1/64.0)
+              if NFA < 1 :
+                  # forgerMask[np.transpose(R)]=True
+                  for i,j in R:
+                      forgerMask[i,j]=True
+              for i,j in R :
+                  votes[i,j] = -1
+                  # forgerMask[np.transpose(R)]=True
+  return forgerMask
 
 
 
 if __name__=="__main__":
   image=cv2.imread("./im2gimp.jpg", cv2.IMREAD_UNCHANGED)
   image=image[:,4:]
+  print(image.shape)
   G,value,votes = gridDetection(image)
   if G==-1 :
-      print("ERREUR")
+      print("No grid detected.")
   else :
       print(f"Meilleur vote {G}")
       mask = forgeryDetection(votes,G,12)
